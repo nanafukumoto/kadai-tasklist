e59d3072f5698591b7d21a891e5dce6fb996f878
nana.fukumoto <naaaaachi1101@gmail.com>
2018-06-06 07:16:43 +0000

    bootstrap design rewrite2

--- resources/views/tasklists/edit.blade.php
+++ resources/views/tasklists/edit.blade.php
@@ -5,7 +5,7 @@
     <h1>id: {{ $tasklist->id }} のタスク編集ページ</h1>
 
  <div class="row">
-        <div class="col-xs-6 col-sm-offset-2 col-sm-8 col-lg-offset3 col-lg-offset-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset3 col-lg-offset-6">
     {!! Form::model($tasklist, ['route' => ['tasklists.update', $tasklist->id], 'method' => 'put']) !!}
 <div class="form-group">
         {!! Form::label('status','ステータス:') !!}
