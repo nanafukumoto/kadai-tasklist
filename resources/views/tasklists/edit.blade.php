dcc008b1848c8599779cfd248c11a2719996dc32
nana.fukumoto <naaaaachi1101@gmail.com>
2018-06-06 06:04:30 +0000

    bootstrap design

--- resources/views/tasklists/edit.blade.php
+++ resources/views/tasklists/edit.blade.php
@@ -4,16 +4,18 @@
 
     <h1>id: {{ $tasklist->id }} のタスク編集ページ</h1>
 
+ <div class="row">
+        <div class="col-xs-6">
     {!! Form::model($tasklist, ['route' => ['tasklists.update', $tasklist->id], 'method' => 'put']) !!}
-
+<div class="form-group">
         {!! Form::label('status','ステータス:') !!}
-        {!! Form::text('status') !!}
-
-
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+ </div>
+        {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
     {!! Form::close() !!}
 
